6e45390df858e7bd783b69bbfbc52d4bf05dfb2a
Immortal_.18 <162115903+ChanikyaMysur@users.noreply.github.com>
2025-12-17 14:16:16 +0530

    Add files via upload

---
+++ TOC_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    length = strlen(str);
+    if (str[0] == "a" && str[llength-1] == "a") {
+            printf("accepted\n");
+    } else {
+        printf("rejected\n");
+    }
+}
